ab7d5fd894b34fa9f49ebbc2b40631783ac1c6f0
tomas <143470010+tomas3167@users.noreply.github.com>
2023-10-04 20:55:10 -0300

    Add files via upload

---
+++ Clase0.py
@@ -0,0 +1,12 @@
+# (esto es un comentario) Input sirve para que el usuario ingrese los datos
+nombre = input ("Ingrese su nombre: ")
+rol = input ("Ingrese su rol: ")
+edad = input ("Ingrese su edad: " )
+fecha = input ("Ingrese su fecha: ")
+
+# print f me permite llamar las variables entre llaves
+print (f"Hola, mi nombre es {nombre}, soy el {rol} y tengo {edad} años y nací el {fecha} ")
+
+# también se puede poner print ("Hola, mi nombre es " , nombre , " soy el " , rol , " y tengo " , edad , " años 
+# y nací el " , fecha ,)
+
---
+++ Clase0_Ej1.py
@@ -0,0 +1,30 @@
+# Declarar 3 variables con valores ingresados por el usuario (numeros) y mostrar en un PRINT el promedio de los números
+
+# int sirve para que el valor ingresado sea numérico, si no pongo eso va a ser texto o cadena
+num1 = int(input ("Ingrese el primer número"))
+num2 = int(input ("Ingrese el segundo número"))
+num3 = int(input ("Ingrese el tercer número"))
+
+promedio = (num1 + num2 + num3) / 3
+
+print (f"{promedio}")
+
+# Comandos: con alt + ctrl y la flecha para abajo o arriba hace que cuando escribo algo se escribe en todos los lados
+# Comandos: con shift + alt y alguna flecha copio y pego la línea
+
+nombre = "tomas"
+existe = False
+edad = 19
+numDecimal = 3.5
+
+print (type(nombre))
+print (type(existe))
+print (type(edad))
+print (type(numDecimal))
+# TYPE sirve para ver el tipo de dato de una variable y la estructura es: print (type(nombre de la variable))
+
+# TODAS las clases
+# str = cadena
+# bool = boleano, True o False siempre con mayúsculas
+# int = número entero
+# float = númedo decimal
