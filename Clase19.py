4f4f4581ec5c0426b0967b02c384bbfdbb303a2d
tomas <143470010+tomas3167@users.noreply.github.com>
2023-11-08 21:18:19 -0300

    Add files via upload

---
+++ Clase19.py
@@ -0,0 +1,14 @@
+#Ejercicio con SET
+#Encontrar la union de los conjuntos
+
+# set1 = {1,2,3,4,5}
+# set2 = {3,4,5,6,7}
+# resultado = set()
+   # resultado.update(set1,set2) Esta es una forma
+# for item in set1:
+#     resultado.add(item)
+# for item in set2:
+#     resultado.add(item)
+   # resultado = set1.union(set2) Esta es otra forma
+# print(resultado)
+ 
