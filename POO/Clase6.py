63c7b558cb105a34637186879aea64c69ae6a226
tomas3167 <tomasbanga12@gmail.com>
2024-04-25 19:16:46 -0300

    Clase6

---
+++ POO/Clase6.py
@@ -0,0 +1,20 @@
+ #Cual seria la logica para evaluar un numero y si es este mayor o igual a 6, mostrar el mensaje "Verde" y sino mostrar el mensaje "Rojo"
+
+ #Variables
+ #En python la posibilidad de declarar variables vacias, es decir, con valor "None"
+# nota = 8
+# color = None
+# if nota >= 6:
+#     print("Verde")
+# else:
+#     print("Rojo")
+
+ #OPERADOR TERNARIO: Nos permite evaluar una condicion de manera abreviada
+# nota = 7
+# color = "Verde" if nota >= 6 else "Rojo"
+# print(nota, color)
+
+ #Dado un numero entero, imprimir "Es par" si el numero es par, sino "Es impar"
+num = 6
+par_impar = "Es par" if num%2 == 0 else "Impar"
+print(num, par_impar)
