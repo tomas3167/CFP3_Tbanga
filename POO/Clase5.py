f36378d899d7d0b427b2dd5ceac4420895d1815a
tomas3167 <tomasbanga12@gmail.com>
2024-04-24 21:27:01 -0300

    Clase5

--- POO/Clase5.py
+++ POO/Clase5.py
@@ -24,11 +24,13 @@
 #     print(f"el resultado es {resultado}")
 
     ### OTRA FORMA
- 
+ # 4- Lo mismo pero guardando cada suma
+
 def suma(num1,num2):
     resultado = num1 + num2
     return resultado
 
+lista_num = []
 def suma_bucle():
     while True:
         num1 = int(input("Ingrese el primer numero (0 para salir) "))
@@ -38,5 +40,26 @@ def suma_bucle():
             break
         resultado_suma = suma(num1,num2)
         print(f"La suma es {resultado_suma}")
+        lista_num.append(resultado_suma)
 suma_bucle()
-#----
+# print(lista_num)
+
+for i, lista_num in enumerate(lista_num, start = 1):
+    print(f"Suma {i}: {lista_num} ")
+# valor total y promedio de las sumas
+print(f"El promedio es s")
+print(f"El total de las sumas es {sum(lista_num)}")
+
+
+# ENUMERATE: Es una funcion en Python que se utiliza para enumerar elementos de una secuencia (lista, tuplas, etc), mientras los recorre en un bucle. ENUMERATE tomas dos parametros: la secuencia que se quiere sumar y opcionalmente el indice inicial. Devuelve un objeto enumerado que produce tuplas que contienen un contador y un elemento de la secuencia.
+
+# frutas = ["Manzana", "Banana", "Pera", "Anana", "Naranja"]
+
+# for indice, fruta in enumerate (frutas, start = 1):
+#     print(f"Fruta {indice}: {fruta}")
+ # OTRA FORMA DE HACERLO CON UN ACUMULADOR
+# acum = 0
+# for i in (frutas):
+#     acum = acum + 1
+#     print(f"Fruta {acum}: {i}")
+
