a762dcc95874ea6bc8d14e5ff7e94fe4e688b552
tomas <143470010+tomas3167@users.noreply.github.com>
2024-07-03 18:00:22 -0300

    Delete POO/Clase23/administration/models.py

--- POO/Clase23/administration/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Position(models.Model):
-    name = models.CharField(max_length=250)
-
-    def __str__(self):
-        return self.name
-
-# Create your models here.
