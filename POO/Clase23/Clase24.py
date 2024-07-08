d5fd2e4d3ddd36056c62493d761c78feeceef0ec
tomas <143470010+tomas3167@users.noreply.github.com>
2024-07-08 19:15:16 -0300

    Update Clase24.py

--- POO/Clase23/Clase24.py
+++ POO/Clase23/Clase24.py
@@ -1,3 +1,4 @@
 # El modelo (base de datos - logica - visual) (3 partes) se llama MVC. En DJANGO se llama MVT
 # M = BD, V = visual, C = logica
+# En Django, M = modelos, V = logica, T = visual
 # Como en la clase 18
