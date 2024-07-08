62e8281af044109100526e1b21a94257b930fe34
tomas <143470010+tomas3167@users.noreply.github.com>
2024-07-08 20:37:50 -0300

    Update Clase24.py

--- POO/Clase23/Clase24.py
+++ POO/Clase23/Clase24.py
@@ -1,4 +1,6 @@
 # El modelo (base de datos - logica - visual) (3 partes) se llama MVC. En DJANGO se llama MVT
 # M = BD, V = visual, C = logica
 # En Django las carpetas son: M = models (BaseD.), V = views (logica), T = templates(visual)
+# La carpeta 'templates' hay que crearla manualmente dentro del proyecto, porque python esta diseñado para
+# backend por eso no la crea automaticamente
 # Como en la clase 18
