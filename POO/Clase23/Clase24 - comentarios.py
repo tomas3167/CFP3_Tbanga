f6d78ab2b8212a5967654dc42a07117465203892
tomas <143470010+tomas3167@users.noreply.github.com>
2024-07-11 19:27:48 -0300

    Update Clase24 - comentarios.py

--- POO/Clase23/Clase24 - comentarios.py
+++ POO/Clase23/Clase24 - comentarios.py
@@ -2,10 +2,10 @@
 # Como en la clase 18
 # M = BD, V = visual, C = logica
 # En Django las carpetas son: M = models (BaseD.), V = views (logica), T = templates(visual)
+
 # La carpeta 'templates' hay que crearla manualmente dentro del proyecto, porque python esta diseñado para
 # backend por eso no la crea automaticamente
 # para crear automaticamente el codigo basico en html hay que poner !
-
 # para poner comentarios en html seleccionas la linea y vas a edit y clickeas en toggle line comment
 # <!-- si ponemos {% %} podemos escribir en lenguaje python en un archivo html -->
 
